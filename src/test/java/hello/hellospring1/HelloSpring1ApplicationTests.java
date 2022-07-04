5b1669b07c6f8e3422748ee0c0c410012f2c9643
mju-jsh <jangso825@naver.com>
2022-07-04 13:31:57 +0900

    Initial commit

---
+++ src/test/java/hello/hellospring1/HelloSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
